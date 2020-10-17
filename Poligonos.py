29998a6b9c6bedf80687f1eae5ec9c5b49062652
Mario Armando Marin Barradas <MarioMarin_25@outlook.com>
2020-10-17 09:55:38 -0500

    Update .gitignore

--- Poligonos.py
+++ Poligonos.py
@@ -1,9 +1,9 @@
 import turtle
 
 class Poligono:
-    def __init__(self, lados, nombre, largo=100, color="black", grosor=3):
-        self.lados = lados
+    def __init__(self, nombre, lados, largo=100, color="black", grosor=3):
         self.nombre = nombre
+        self.lados = lados
         self.largo = largo
         self.color = color
         self.grosor = grosor
@@ -11,32 +11,23 @@ class Poligono:
     def dibujar(self):
         turtle.color(self.color)
         turtle.pensize(self.grosor)
-        for i in range(self.lados):
+        for lado in range(self.lados):
             turtle.forward(self.largo)
             turtle.right(360 / self.lados)
 
 class Cuadrado(Poligono):
     def __init__(self, largo=100, color="black", grosor=3):
-        super().__init__(4, "cuadrado", largo, color, grosor)
+        super().__init__("Cuadrado", 4, largo, color, grosor)
 
-class Hexagono(Poligono):
-    def __init__(self, largo=100, color="black", grosor=3):
-        super().__init__(6, "hexagono", largo, color, grosor)
-
-    def dibujar(self):
+    def dibujar_lleno(self):
         turtle.begin_fill()
         super().dibujar()
         turtle.end_fill()
 
-# Poligonos Objeto
-cuadrado = Poligono(4, "cuadrado", 150, "blue", 5)
-pentagono = Poligono(5, "pentagono")
-hexagono = Poligono(6, "haxagono", grosor=20, color="red")
-icosagono = Poligono(20, "icosagono", 25)
 
-# Poligonos Clase
-cuadrado = Cuadrado(200, "red", 1)
-hexagono = Hexagono(color="blue", grosor=5)
+cuadrado_1 = Poligono("Cuadrado", 4)
+cuadrado_2 = Cuadrado(color="Blue", grosor=5)
 
-hexagono.dibujar()
+cuadrado_2.dibujar_lleno()
+
 turtle.done()
